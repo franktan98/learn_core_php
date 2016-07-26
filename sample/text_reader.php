ab35f3433899cbb115140e4fe0fc90bdc4085f79
franktan98 <franktan98@yahoo.com>
2016-07-27 01:24:09 +0800

    testing on reading text file

---
+++ sample/text_reader.php
@@ -0,0 +1,15 @@
+<?php
+    defined('SAFE_CALL') OR define('SAFE_CALL',true);
+    include_once '../library/Simple_Text.php' ;
+
+    use SimpleLibrary\Simple_Text ;
+    
+    $text_reader = new Simple_Text();
+    echo '<pre>';
+    echo print_r($text_reader->read_text_file('', 'sample.ini')); 
+    echo '</pre>';
+    echo '<pre>';
+    echo print_r($text_reader->read_speacial_text('', 'sample.ini',true)); 
+    echo '</pre>';
+?>
+
