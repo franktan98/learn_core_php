5858d10380bd552cc9d324e6621850c926bcd52f
franktan98 <franktan98@yahoo.com>
2016-07-26 23:28:31 +0800

    simple link page to sample created

---
+++ a.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * nothing just a link to sample create before
+ */
+
+?>
+<html>
+    <head><title>Sample link</title></head>
+    <body>
+        <div id="container" style="overflow:hidden;width:1300px">
+            <div id="inner" style="overflow:hidden;width: 1250px">
+               <div style="float:left;width:0px;height:600px">
+                    <pre>
+                        <a href="sample/date.php" target="sample">Date time formating </a>
+                        Others coming soon sample
+                    </pre>
+               </div>
+                
+               <div style="float:right;width:850px;height:600px">
+                    <iframe name="sample" src="sample/date.php" style="float:left;width:850px;height:600px"></iframe> 
+               </div>
+            </div>
+        </div>        
+        <div>
+        </div>
+    </body>
+</html>
