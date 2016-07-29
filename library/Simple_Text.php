f6766c3110e28f7e6bb42b34443c68b7564e5f99
franktan98 <franktan98@yahoo.com>
2016-07-30 00:04:41 +0800

    Write and apend to file.

--- library/Simple_Text.php
+++ library/Simple_Text.php
@@ -7,6 +7,7 @@
  */
 
 namespace SimpleLibrary;
+
 defined('SAFE_CALL') OR exit('No direct script access allowed');
 
 /**
@@ -15,62 +16,67 @@ defined('SAFE_CALL') OR exit('No direct script access allowed');
  * @author frank
  */
 class Simple_Text {
-    
+
     private $file_name;
     private $store_directory;
-    
+
     /**
      * initiliaze of the class
      * set to default setting ready to use mode
      */
-    private function init_class(){
+    private function init_class() {
         $this->file_name = 'default.txt';
         $this->store_directory = '';
     }
-    
+
     public function __construct() {
         $this->init_class();
     }
-    
-    public function read_speacial_text($source_directory = '' , $source_file_name = 'default.txt', $source_issource = false ){
-        $return_value = '' ; 
-        $file_read = $source_directory .''.$source_file_name ;
-        try{
-            if ($source_issource){
-                $return_value = show_source($file_read) ;
-            }else{
-                // if the text is in <> then thiswill skip to reading those text inside the <> block
-                $return_value = file_get_contents($file_read);
-            }
-        } catch (Exception $ex) {
-            $return_value = 'file not found or reading permission deny.';
+
+    public function read_speacial_text($source_directory = '', $source_file_name = 'default.txt', $source_issource = false) {
+        $return_value = '';
+        $file_read = $source_directory . '' . $source_file_name;
+        if ($source_issource) {
+            $return_value = show_source($file_read) or die($return_value = $file_read . ' file not found or reading permission deny.');
+        } else {
+            // if the text is in <> then thiswill skip to reading those text inside the <> block
+            $return_value = file_get_contents($file_read) or die($return_value = $file_read . ' file not found or reading permission deny.');
         }
-        return $return_value ; 
+        return $return_value;
     }
-    
-    public function read_text_file($source_directory = '' , $source_file_name = 'default.txt'){
-        $return_value = '' ; 
-        $file_read = $source_directory .''.$source_file_name ;
-        try{
-            $myfile = fopen($file_read,'r') or die($return_value = $file_read. ' file not found or reading permission deny.');
-            while(! feof($myfile))
-            {
-                $return_value[] = fgets($myfile) ;
-            }
-            fclose($myfile);
-        } catch (Exception $ex) {
-            $return_value = 'file not found or reading permission deny.';
+
+    public function read_text_file($source_directory = '', $source_file_name = 'default.txt') {
+        $return_value = '';
+        $file_read = $source_directory . '' . $source_file_name;
+        $myfile = fopen($file_read, 'r') or die($return_value = $file_read . ' file not found or reading permission deny.');
+        while (!feof($myfile)) {
+            $return_value[] = fgets($myfile);
         }
-        return $return_value ; 
+        fclose($myfile);
+        return $return_value;
     }
-    
-    public function write_file(){
+
+    public function write_file($source_directory , $source_file_name, $source_contant,$append) {
+        $return_value = '';
+        $file_write = $source_directory . '' . $source_file_name;
+        if ( $append ){
+            $myfile = fopen($file_write, 'w') 
+                    or die($return_value = $file_write . ' file not found or reading permission deny.');
+        }else{
+            $myfile = fopen($file_write, 'a') 
+                    or die($return_value = $file_write . ' file not found or reading permission deny.');
+        }
+        echo $source_contant;
+        fputs($myfile , $source_contant);
+        fclose($myfile);
+        
+        return $return_value;
+    }
+
+    public function append_file() {
         
     }
-    
-    public function append_file(){
-        
-    }
+
 }
 
 /*
--- sample/text_reader.php
+++ sample/text_reader.php
@@ -4,12 +4,17 @@
 
     use SimpleLibrary\Simple_Text ;
     
+    const NEWLINE = "\r\n";
+
     $text_reader = new Simple_Text();
     echo '<pre>';
     echo print_r($text_reader->read_text_file('', 'sample.ini')); 
     echo '</pre>';
     echo '<pre>';
-    echo print_r($text_reader->read_speacial_text('', 'samplsae.ini',true)); 
+    echo print_r($text_reader->read_speacial_text('', 'date.php',true)); 
     echo '</pre>';
-?>
+    $content = '1st line.' . "\r\n" ; 
+    $content = $content .'2nd line.' . "\r\n" ; 
+    $content = $content .'3rd line.' . "\r\n" ; 
+    $text_reader->write_file('', 'sample.txt',$content,false);
 
