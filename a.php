fa71f8e484776aec7f225b259bf6773eb456a871
franktan98 <franktan98@yahoo.com>
2016-07-30 10:00:25 +0800

    change the manu layout

--- a.php
+++ a.php
@@ -2,26 +2,47 @@
 /**
  * nothing just a link to sample create before
  */
-
 ?>
 <html>
     <head><title>Sample link</title></head>
     <body>
         <div id="container" style="overflow:hidden;width:1300px">
             <div id="inner" style="overflow:hidden;width: 1250px">
-               <div style="float:left;width:0px;height:600px">
-                    <pre>
-                        <a href="sample/date.php" target="sample">Date time formating </a>
-                        <a href="sample/text_reader.php" target="sample">text reader</a>
-                        <a href="sample/directory.php" target="sample">Directory and File</a>
-                        <a href="sample/loop_control.php" target="sample">loop control</a>
-                        Others coming soon sample
-                    </pre>
-               </div>
-                
-               <div style="float:right;width:850px;height:600px">
+                <div style="float:left;width:300px;height:1600px">
+                    <br/><a href="sample/date.php" target="sample">2016 Jul -- Date time formating </a>
+                    <br/><a href="sample/text_reader.php" target="sample">2016 Jul -- text reader</a>
+                    <br/><a href="sample/loop_control.php" target="sample">2016 Jul 29 loop control</a>
+                    <br/><a href="sample/mysqli_sample.php" target="sample">2016 Jul 30 loop control</a>
+                    <br/>
+                    Pending
+                    <br/>
+                    <a href="sample/directory.php" target="sample">Directory and File</a>
+                    <br/>
+                    Sorting Algoritham
+                    <ul>
+                        <li>Sort</li>
+                    </ul>
+
+                    GOF Design Pattern
+                    <ul>
+                        <li>Creation</li>
+                        <li>Creation</li>
+                        <li>Creation</li>
+                    </ul>
+
+                    Data Structure
+                    <ul>
+                        <li>Queue</li>
+                        <li>Stack</li>
+                        <li>Hash</li>
+                        <li>Tree</li>
+                        <li></li>
+                    </ul>
+                </div>
+
+                <div style="float:right;width:850px;height:600px">
                     <iframe name="sample" src="sample/date.php" style="float:left;width:850px;height:600px"></iframe> 
-               </div>
+                </div>
             </div>
         </div>        
         <div>
--- sample/loop_control.php
+++ sample/loop_control.php
@@ -4,8 +4,39 @@
     $begin_of_the_loop = 0 ;
     $total_loop = 15 ;
 
-    for($counter_for_loop = $begin_of_th_loop ;
-        $counter_for_loop > $total_loop ;$counter_for_loop++ ){
-            echo $counter_for_loop  ; 
+//======================================================
+    echo '<br />for Loop<br />';
+    for($counter_for_loop = $begin_of_the_loop ;
+        $counter_for_loop < $total_loop ;
+        $counter_for_loop++ ){
+            echo $counter_for_loop ; 
     }
+//======================================================
+    echo '<br />while Loop<br />';
+    $counter_for_while = 0 ;
+    while($counter_for_while < $total_loop){
+        echo $counter_for_while ; 
+        $counter_for_while++;
+    }
+//======================================================
+    echo '<br />do while Loop<br />';
+    $counter_do_loop = 0 ;
+    do{
+        echo $counter_do_loop ; 
+        $counter_do_loop++;
+    }while($counter_do_loop > $total_loop);
+//======================================================
+    echo '<br />do while Loop<br />';
+    $counter_do_loop = 0 ;
+    do{
+        echo $counter_do_loop ; 
+        $counter_do_loop++;
+    }while($counter_do_loop < $total_loop);
+    echo '<br />';
+//======================================================
+//if then else            
+
+//======================================================
+// switch
+    
     
