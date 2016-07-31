a2bdc743ee34cf7a24f3867a0897c306c0552dad
franktan98 <franktan98@yahoo.com>
2016-07-31 22:58:20 +0800

    Encapsulation sample

--- sample/ObjectOriented/Human.php
+++ sample/ObjectOriented/Human.php
@@ -1,8 +1,37 @@
 <?php
+namespace OO_Sample ; 
+// Basic Object Oriented programming Encapsulation
+//a simple class to show how Encapsulation of object oriented programming with php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Human {
 
+    private $name;
+    private $date_of_birth;
+    private $gendar;
+
+    public function __construct($source_name, $source_date_of_birth, $source_gender) {
+        $this->name = $source_name;
+        $this->date_of_birth = $source_date_of_birth;
+        $this->gendar = $source_gender;
+    }
+
+    public function __get($source_property) {
+        $method = "get_$source_property";
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+
+    public function get_name() {
+        return $this->name;
+    }
+
+    public function get_date_of_birth() {
+        return $this->date_of_birth;
+    }
+
+    public function get_gendar() {
+        return $this->gendar;
+    }
+
+}
