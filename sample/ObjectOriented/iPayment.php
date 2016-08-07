326c60a2e69adeaab7e4cf79f0b432f51ae11418
QQ Tan <franktan98@yahoo.com>
2016-08-08 00:27:14 +0800

    Delete iPayment.php

--- sample/ObjectOriented/iPayment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface iPayment{
-    public function payment_request($source_date,$source_staff_id,$source_amount,$source_reson);
-    public function payment_aprovel($source_approver, $payment_id );
-    public function payment_reject($source_reject_by, $payment_id, $source_reson );
-    public function payment_made($source_date,$payment_method);
-}
